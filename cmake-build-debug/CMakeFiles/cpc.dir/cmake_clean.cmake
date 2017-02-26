73456276f17a745a243e538243653cab4a4075b1
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2017-02-26 17:29:34 -0600

    initial clion project

---
+++ cmake-build-debug/CMakeFiles/cpc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpc.dir/main.cpp.o"
+  "cpc.pdb"
+  "cpc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
