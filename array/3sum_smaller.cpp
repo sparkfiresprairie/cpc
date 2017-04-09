e8ba5a6b9b30869ad7134a3403b28b0c75c7a04c
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2017-04-08 23:29:50 -0500

    04/08/2017

--- array/3sum_smaller.cpp
+++ array/3sum_smaller.cpp
@@ -22,7 +22,7 @@ int threeSumSmaller(vector<int>& nums, int target) {
     int count = 0;
     sort(nums.begin(), nums.end());
     int n = nums.size();
-    for (int i = 0; i < n - 2; ++i) {
+    for (int i = 0; i + 2 < n; ++i) {
         int left = i + 1, right = n - 1;
         while (left < right) {
             if (nums[i] + nums[left] + nums[right] < target) {
--- dynamic_programming/word_break.cpp
+++ dynamic_programming/word_break.cpp
@@ -23,7 +23,10 @@ bool wordBreak(string s, vector<string>& wordDict) {
     int n = s.size();
     vector<bool> dp(n + 1, false);
     dp[0] = true;
+    // i is the size of the string
     for (int i = 1; i <= n; ++i) {
+        // j is the cut point
+        // [0, j), [j, i)
         for (int j = 0; j < i; ++j) {
             if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
                 dp[i] = true;
